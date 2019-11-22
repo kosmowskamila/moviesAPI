e92c2dc097dd7332f9f2aeb4008264124d36116d
kamkulas <kam.kulas@gmail.com>
2019-11-22 11:58:44 +0100

    Filtering comments by movie id.

--- comment/views.py
+++ comment/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
+
 from comment.models import Comment
 from comment.serializers import CommentSerializer
 
@@ -6,4 +8,5 @@ from comment.serializers import CommentSerializer
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = ['movie']
